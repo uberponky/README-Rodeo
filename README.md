85c3340c1510b5b8d12dba40758e52ba1e741c17
Rory Simmonds <rory@blankbox.business>
2024-02-16 21:48:22 +0000

    add question logic and await block

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# README-Rodeo
-A command-line application to quickly generate a professional README file
+# a
+
+## Description
+a
+
+## Table of Contents
+[Installation](#installation)
+[Usage](#usage)
+[License](#license)
+[Contribution](#contribution)
+[Tests](#tests)
+[Questions](#questions)
+
+## Installation
+a
+
+## Usage
+a
+
+## License
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+MIT
+
+## Contribution
+a
+
+## Tests
+a
+
+## Questions
+Please sent all questions to:
+a - a
+
+[My GitHub profile](a)
--- index.js
+++ index.js
@@ -15,11 +15,70 @@ const questions = [
 
 // function to write README file
 function writeToFile(fileName, data) {
+  fs.writeFile(path.join(__dirname,fileName), data, function (err) {
+    if (err) throw err;
+    console.log('Saved!')
+  })
 }
 
 // function to initialize program
-function init() {
-  generateMarkdown('test')
+async function init() {
+  const userInput = await inquirer.prompt([
+    {
+      name: "username",
+      message: "Enter your GitHub username:",
+      type: "input",
+    },
+    {
+      name: "email",
+      message: "Enter your GitHub email:",
+      type: "input",
+    },
+    {
+      name: "url",
+      message: "Enter your GitHub account URL:",
+      type: "input",
+    },
+    {
+      name: "title",
+      message: "Enter your project title:",
+      type: "input",
+    },
+    {
+      name: "description",
+      message: "Enter your project description:",
+      type: "input",
+    },
+    {
+      name: "installation",
+      message: "Enter your installation instructions:",
+      type: "input",
+    },
+    {
+      name: "usage",
+      message: "Enter your usage instructions:",
+      type: "input",
+    },
+    {
+      name: "license",
+      message: "Select a license",
+      type: "list",
+      choices: ['MIT', "Apache 2.0", "Boost 1.0", "BSD 3-Clause", "BSD 2-Clause"]
+    },
+    {
+      name: "contribution",
+      message: "Enter your contribution instructions:",
+      type: "input",
+    },
+    {
+      name: "test",
+      message: "Enter the command that should be used to run tests:",
+      type: "input",
+    }
+  ])
+
+  const markdown = generateMarkdown(userInput)
+  writeToFile('README.md', markdown)
 }
 
 // function call to initialize program
